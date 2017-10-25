f50542723256f82a84b2bf176434c844a1171074
Dallin Skinner <dskinner@verisage.us>
2017-10-24 22:15:34 -0600

    email checkout

---
+++ client/src/scenes/Shop/products.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    image: [
+      require('./img/cta-buttons.jpg')],
+    name: 'CTA Buttons',
+    price: 5,
+    description: 'One little baggy stuffed to the rafters with 4-6 buttons. We Don’t really know how many to be honest. But they’ll mostly be different. Or at least you’d hope so.',
+  }, {
+    image: [
+      require('./img/56kbps-front.jpg'),
+      require('./img/56kbps-back.jpg')],
+    name: '56kbps Shirt',
+    price: 15,
+    description: 'The 56kbps is an embroidered shirt with a small filled hit on the front left and an outlined hit across the back. If you don’t know what 56kbps means, dial up a friend and ask. Or don’t. No problem.',
+  }, {
+    image: [
+      require('./img/cursor-patch.jpg')],
+    name: 'Cursor Patch',
+    price: 3,
+    description: 'The Loser Interface patch is as cool as it gets. That is, if you still like Digimon and can’t find Carmen Sandiego ya nerd. Put it on a shirt or a hat. Or maybe buy it and let it sit in a drawer for a while \'til you remember it’s there.',
+  },
+];
