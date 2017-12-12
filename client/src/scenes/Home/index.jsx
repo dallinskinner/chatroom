8fb82d431b39cd85a1872d915b502287854ec83e
Dallin Skinner <dskinner@verisage.us>
2017-12-12 11:00:31 -0700

    Added gifs on home page

--- client/src/scenes/Home/index.jsx
+++ client/src/scenes/Home/index.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 
+import styles from './styles.scss';
+
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      index: 0,
+      images: [
+        require('./img/work-stuff.gif'),
+        require('./img/click-here.gif'),
+      ],
+    };
+
+    this.nextGif = this.nextGif.bind(this);
   }
 
   componentDidMount() {
     this.props.updateTitle('Loser Interface');
   }
 
+  nextGif() {
+    let index = this.state.index + 1;
+
+    if (index === this.state.images.length) {
+      index = 0;
+    }
+
+    this.setState({
+      index: index,
+    });
+  }
+
   render() {
-    return null;
+    return (
+      <div className={styles.wrapper}>
+        <button className={styles.button} onClick={this.nextGif}>
+          <img
+            className={styles.gif}
+            src={this.state.images[this.state.index]} />;
+        </button>
+      </div>
+    );
   }
 }
